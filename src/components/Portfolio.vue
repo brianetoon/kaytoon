e0e43b32b14b5be2a3a4846373797fe771a3329b
Brian <brian.elliot.toon@gmail.com>
2021-02-02 14:45:29 -0500

    portfolio component

---
+++ src/components/Portfolio.vue
@@ -0,0 +1,144 @@
+<template>
+    <div id="portfolio">
+      <router-link to="/" class="project" v-for="project in projects" :key="project.id">
+            <img :src="require(`@/assets/images/${project.image}`)">
+            <div class="overlay">
+                <div class="overlay-text">{{ project.title }}</div>
+            </div>
+      </router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['projects']
+}
+</script>
+
+<style>
+#portfolio {
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+}
+.project {
+  grid-column: span 6;
+  position: relative;
+}
+.project img {
+  display: block;
+  width: 100%;
+}
+
+.overlay-text {
+  color: white;
+  font-size: 20px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+.overlay {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0;
+  transition: .5s ease;
+  background-image: url('../assets/overlay.png');
+  background-size: contain;
+}
+.project:hover .overlay {
+  opacity: 1;
+}
+
+
+@media screen and (min-width: 900px) {
+  .project {
+    grid-column: span 4;
+  }
+  #portfolio .project:nth-child(4) {
+    grid-column: 9/13;
+    grid-row: 1;
+  }
+  #portfolio .project:nth-child(5) {
+    grid-column: 1/5;
+    grid-row: 2;
+  }
+    #portfolio .project:nth-child(8) {
+    grid-column: 5/9;
+    grid-row: 2;
+  }
+  #portfolio .project:nth-child(9) {
+    grid-column: 1/5;
+    grid-row: 3;
+  }
+  #portfolio .project:nth-child(7) {
+    grid-column: 5/9;
+    grid-row: 3;
+  }
+  #portfolio .project:nth-child(11) {
+    grid-column: 1/5;
+    grid-row: 4;
+  }
+  #portfolio .project:nth-child(12) {
+    grid-column: 5/9;
+    grid-row: 4;
+  }
+}
+
+@media screen and (min-width: 1200px) {
+  .project {
+    grid-column: span 3;
+  }
+  #portfolio .project:nth-child(1) {
+    grid-column: 1/4;
+    grid-row: 1;
+  }
+  #portfolio .project:nth-child(2) {
+    grid-column: 4/7;
+    grid-row: 1;
+  }
+  #portfolio .project:nth-child(4) {
+    grid-column: 7/10;
+    grid-row: 1;
+  }
+  #portfolio .project:nth-child(8) {
+    grid-column: 10/13;
+    grid-row: 1;
+  }
+  #portfolio .project:nth-child(3) {
+    grid-column: 1/4;
+    grid-row: 2;
+  }
+  #portfolio .project:nth-child(5) {
+    grid-column: 4/7;
+    grid-row: 2;
+  }
+  #portfolio .project:nth-child(7) {
+    grid-column: 10/13;
+    grid-row: 2;
+  }
+  #portfolio .project:nth-child(10) {
+    grid-column: 1/4;
+    grid-row: 3;
+  }
+  #portfolio .project:nth-child(9) {
+    grid-column: 4/7;
+    grid-row: 3;
+  }
+  #portfolio .project:nth-child(11) {
+    grid-column: 7/10;
+    grid-row: 3;
+  }
+  #portfolio .project:nth-child(12) {
+    grid-column: 10/13;
+    grid-row: 3;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <Portfolio :projects="projects" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import store from '@/store.js'
+import Portfolio from '@/components/Portfolio.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  components: { Portfolio },
+  data() {
+    return {
+      projects: store.projects
+    }
   }
 }
 </script>
+
+<style>
+
+</style>
